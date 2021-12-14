a7b832d1d256aa2981fab13010d3d924209ae381
LukeHuckman <lukeh@outlook.my>
2021-12-15 02:21:19 +0800

    Prepare main activity, navgraph, about fragment.

---
+++ app/src/main/java/com/g3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.g3;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DrawerLayout drawerLayout = findViewById(R.id.DLMain);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle
+                (this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void setupNavMenu(NavController navController)
+}
